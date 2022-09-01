1b020e680dda3218d5cbf77c2092961939e11a04
aravindaka14 <aravindaka14@gmail.com>
2022-09-01 18:38:44 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import '../styles/App.css';
+
+function App() {
+
+    const [input, setInput] = useState("")
+    const [list, setList] = useState([])
+    const handlelist = (e) => {
+        e.preventDefault()
+        return (
+            setList([...list, input]), setInput("")
+        )
+    }
+    return (
+        <div id="main">
+            {/* Do not remove the main div */}
+            <h1>Relative List</h1>
+            <div className="content">
+                <form onSubmit={handlelist}>
+                    <input type="text" onChange={(e) => { setInput(e.target.value) }} value={input} required />
+                    <button type="submit" >Add name</button>
+                </form>
+                <ol key="relativeList">
+                    {
+                        list.map((ele, i = `relativeList${i}`) => {
+                            return (
+                                <li className="list" key={i}>{ele}</li>
+                            )
+                        })
+                    }
+                </ol>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default App;
